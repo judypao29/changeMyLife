12f4892329cb36d2b8ee5eff241020359b16e8fd
paoc <paoc@umich.edu>
2018-07-07 09:07:56 -0400

    pushing all changes

---
+++ ChangeMyLife.Tests/formTests/loginFormTest.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+// using HtmlAgilityPack;
+
+
+
+namespace ChangeMyLife.Tests.formTests
+{
+    public class LoginFormTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // Htmldocument loginForm = Htmldocument.DocumentNode
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
